9a9378c1c75df5ab1c613f07b48f42fc82d6fb3c
Oliver Benning <obenn009@uottawa.ca>
2020-07-08 19:39:00 -0400

    Remove namspaces

--- build.sh
+++ build.sh
@@ -1,66 +1,80 @@
 #!/bin/sh
 
-echo "Building Default Linux image (Python 3, Precise, 64bit)"
+echo "Building Default Linux image (Python 3.8.3, Precise, 64bit)"
+docker build -f Dockerfile-py3-focal-amd64 \
+--build-arg PYTHON_VERSION 3.8.3
+--build-arg PYINSTALLER_VERSION 3.6
+-t pyinstaller-linux \
+-t pyinstaller-linux \
+-t pyinstaller-linux:64bit \
+-t pyinstaller-linux:python3 \
+-t pyinstaller-linux:python3.8 \
+-t pyinstaller-linux:python3.8.3 \
+-t pyinstaller-linux:focal \
+-t pyinstaller-linux:python3-64bit \
+-t pyinstaller-linux:python3-focal \
+-t pyinstaller-linux:python3-focal -64bit \
+.
+
+echo "Building Linux image (Python 3.7.5, Precise, 64bit)"
 docker build -f Dockerfile-py3-precise-amd64 \
--t $NAMESPACE/pyinstaller-linux \
--t $NAMESPACE/pyinstaller-linux:64bit \
--t $NAMESPACE/pyinstaller-linux:python3 \
--t $NAMESPACE/pyinstaller-linux:precise \
--t $NAMESPACE/pyinstaller-linux:python3-64bit \
--t $NAMESPACE/pyinstaller-linux:python3-precise \
--t $NAMESPACE/pyinstaller-linux:python3-precise-64bit \
+--build-arg PYTHON_VERSION 3.7.5
+--build-arg PYINSTALLER_VERSION 3.6
+-t pyinstaller-linux:precise \
+-t pyinstaller-linux:python3.7 \
+-t pyinstaller-linux:python3.7.5 \
 .
 
 echo "Building Default Windows image (Python 3, Xenial, 64bit)"
 docker build -f Dockerfile-py3-xenial-win64 \
--t $NAMESPACE/pyinstaller-windows \
--t $NAMESPACE/pyinstaller-windows:64bit \
--t $NAMESPACE/pyinstaller-windows:xenial \
--t $NAMESPACE/pyinstaller-windows:python3 \
--t $NAMESPACE/pyinstaller-windows:python3-64bit \
--t $NAMESPACE/pyinstaller-windows:python3-xenial \
--t $NAMESPACE/pyinstaller-windows:python3-xenial-64bit \
+-t pyinstaller-windows \
+-t pyinstaller-windows:64bit \
+-t pyinstaller-windows:xenial \
+-t pyinstaller-windows:python3 \
+-t pyinstaller-windows:python3-64bit \
+-t pyinstaller-windows:python3-xenial \
+-t pyinstaller-windows:python3-xenial-64bit \
 .
 
 echo "Building Default Linux 32 bits image (Python 3, Precise, 32bit)"
 docker build -f Dockerfile-py3-precise-i386 \
--t $NAMESPACE/pyinstaller-linux:32bit \
--t $NAMESPACE/pyinstaller-linux:python3-32bit \
--t $NAMESPACE/pyinstaller-linux:python3-precise-32bit \
+-t pyinstaller-linux:32bit \
+-t pyinstaller-linux:python3-32bit \
+-t pyinstaller-linux:python3-precise-32bit \
 .
 
 echo "Building Default Windows 32 bits image (Python 3, Xenial, 32bit)"
 docker build -f Dockerfile-py3-xenial-win32 \
--t $NAMESPACE/pyinstaller-windows:32bit \
--t $NAMESPACE/pyinstaller-windows:python3-32bit \
--t $NAMESPACE/pyinstaller-windows:python3-xenial-32bit \
+-t pyinstaller-windows:32bit \
+-t pyinstaller-windows:python3-32bit \
+-t pyinstaller-windows:python3-xenial-32bit \
 .
 
 echo "Building Default Windows Python 2 image (Python 2, Trusty, 32bit)"
 docker build -f Dockerfile-py2-trusty-win32 \
--t $NAMESPACE/pyinstaller-linux:python2 \
--t $NAMESPACE/pyinstaller-linux:python2-trusty \
--t $NAMESPACE/pyinstaller-linux:python2-32bit \
--t $NAMESPACE/pyinstaller-linux:python2-trusty-32bit \
+-t pyinstaller-linux:python2 \
+-t pyinstaller-linux:python2-trusty \
+-t pyinstaller-linux:python2-32bit \
+-t pyinstaller-linux:python2-trusty-32bit \
 .
 
 echo "Building Default Linux Python 2 image (Python 2, Precise, 64bit)"
 docker build -f Dockerfile-py2-precise-amd64 \
--t $NAMESPACE/pyinstaller-linux:python2 \
--t $NAMESPACE/pyinstaller-linux:python2-precise \
--t $NAMESPACE/pyinstaller-linux:python2-precise-64bit \
+-t pyinstaller-linux:python2 \
+-t pyinstaller-linux:python2-precise \
+-t pyinstaller-linux:python2-precise-64bit \
 .
 
 echo "Building Default Linux Focal image (Python 3, Focal, 64bit)"
 docker build -f Dockerfile-py3-focal-amd64 \
--t $NAMESPACE/pyinstaller-linux:focal \
--t $NAMESPACE/pyinstaller-linux:python3-focal \
--t $NAMESPACE/pyinstaller-linux:python3-focal-64bit \
+-t pyinstaller-linux:focal \
+-t pyinstaller-linux:python3-focal \
+-t pyinstaller-linux:python3-focal-64bit \
 .
 
 echo "Building Default Linux Xenial image (Python 3, Xenial, 64bit)"
 docker build -f Dockerfile-py3-xenial-amd64 \
--t $NAMESPACE/pyinstaller-linux:xenial \
--t $NAMESPACE/pyinstaller-linux:python3-xenial \
--t $NAMESPACE/pyinstaller-linux:python3-xenial-64bit \
+-t pyinstaller-linux:xenial \
+-t pyinstaller-linux:python3-xenial \
+-t pyinstaller-linux:python3-xenial-64bit \
 .
--- publish-github.sh
+++ publish-github.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 GITHUB_REPOSITORY=$1
-for IMAGE in $(docker images --format '{{.Repository}}:{{.Tag}}' $NAMESPACE); do
+for IMAGE in $(docker images --format '{{.Repository}}:{{.Tag}}' pyinstaller-linux); do
+    GITHUB_IMAGE=docker.pkg.github.com/$GITHUB_REPOSITORY/$IMAGE
+    docker tag $IMAGE $GITHUB_IMAGE
+    docker push $GITHUB_IMAGE
+done
+for IMAGE in $(docker images --format '{{.Repository}}:{{.Tag}}' pyinstaller-windows); do
     GITHUB_IMAGE=docker.pkg.github.com/$GITHUB_REPOSITORY/$IMAGE
     docker tag $IMAGE $GITHUB_IMAGE
     docker push $GITHUB_IMAGE
