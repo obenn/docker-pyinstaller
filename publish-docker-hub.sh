53340b1950d7f05fa6fc723aed14985ea1656eeb
Oliver Benning <obenn009@uottawa.ca>
2020-07-09 10:29:41 -0400

    Remove extraneous values

--- publish-docker-hub.sh
+++ publish-docker-hub.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 DOCKER_HUB_NAMESPACE=$1
 for IMAGE in $(docker images --format '{{.Repository}}:{{.Tag}}' pyinstaller-linux); do
-    DOCKER_HUB_IMAGE=$DOCKER_HUB_NAMESPACE/$GITHUB_REPOSITORY/$IMAGE
+    DOCKER_HUB_IMAGE=$DOCKER_HUB_NAMESPACE/$IMAGE
     docker tag $IMAGE $DOCKER_HUB_IMAGE
     docker push $DOCKER_HUB_IMAGE
 done
 for IMAGE in $(docker images --format '{{.Repository}}:{{.Tag}}' pyinstaller-windows); do
-    DOCKER_HUB_IMAGE=$DOCKER_HUB_NAMESPACE/$GITHUB_REPOSITORY/$IMAGE
+    DOCKER_HUB_IMAGE=$DOCKER_HUB_NAMESPACE/$IMAGE
     docker tag $IMAGE $DOCKER_HUB_IMAGE
     docker push $DOCKER_HUB_IMAGE
 done
